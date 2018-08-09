4d28c7a0856586e638a139ea1c864a6924a85fbe
Haiyang Kehoe <haiyang.kehoe@gmail.com>
2018-08-09 16:13:09 -0700

    Update header style

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # index.html
-26 July 2018: First commit. Banner design by Jon Topielski. Navbar needs to be fixed (bootstrap, active button). Content needs to be added.
+26 July 2018: First commit. Banner design by Jon Topielski. Navbar needs to be fixed (bootstrap, active button). Content needs to be added.  
 
 # styles.css
-26 July 2018: First commit. Parallax banner header should be text instead of an image.
-08 August 2018: Fixed bug where element size changed when using navbar buttons.
+26 July 2018: First commit. Parallax banner header should be text instead of an image.  
+08 August 2018: Fixed bug where element size changed when using navbar buttons.  
+09 August 2018: Updated header styling.  
 
 # main.js
-26 July 2018: First commit. Refresh on resize is a (temporary?) fix for navbar scaling issues.
+26 July 2018: First commit. Refresh on resize is a (temporary?) fix for navbar scaling issues.  
+09 August 2018: Added function that changes background color on scroll.  
--- main.js
+++ main.js
@@ -1,12 +1,39 @@
+window.onscroll = function() {
+  stickyNav(); //Sticky navbar
+  scrollColor(); //Change background color
+  navScroll(); //Highlight navbar section on scroll
+}
+
 //Sticky navbar
-window.onscroll = function() {stickyNav()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")  
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+  }
+}
+     
+//Change background color
+var header = document.getElementById("header");
+var position = 0.4 * header.offsetHeight;
+function scrollColor() {
+  if (window.pageYOffset >= position) {
+    document.body.classList.add("changeColor");
+  } else {
+    document.body.classList.remove("changeColor");
+  }
+}
+
+//Highlight navbar section on scroll
+var aboutme = document.getElementById("aboutme");
+var projects = document.getElementById("projects");
+var cv = document.getElementById("cv");
+var contact = document.getElementById("contact");
+function navScroll() {
+  if (window.pageYOffset >= aboutme) {
+    navbar.classList.add("active");
   }
 }
 
