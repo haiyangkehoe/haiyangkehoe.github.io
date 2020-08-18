e553167d59c76847c5d18c06d18251c7d89cea0f
Haiyang Kehoe <haiyang.kehoe@gmail.com>
2020-08-18 14:40:30 -0700

    Attempting to fix navbar bug

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ var navbar = document.querySelector("nav")
 var header = document.querySelector("header");
 var sticky = header.offsetHeight;
 var position = 0.44 * header.offsetHeight;
-window.onresize = function() {
+window.addEventListener('resize', function() {
   sticky = header.offsetHeight;
   position = 0.44 * header.offsetHeight;
-}
+})
 window.onscroll = function() {
   stickyNav(); //Sticky navbar
   scrollColor(); //Change background color
 }
+
 //Sticky navbar
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
@@ -19,6 +20,7 @@ function stickyNav() {
     navbar.classList.remove("sticky");
   }
 }
+
 //Change background color
 function scrollColor() {
   if (window.pageYOffset >= position) {
